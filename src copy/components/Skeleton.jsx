923f93b89b1e6cff930997776d86265939e7d37a
unknown <saiimanogn2000@gmail.com>
2026-04-28 12:54:43 +0530

    Skeleton is corrected and changed

--- src copy/components/Skeleton.jsx
+++ src copy/components/Skeleton.jsx
@@ -38,7 +38,7 @@ export const ThumbnailSkeleton = () => {
                   width={80}
                   height={80}
                   variant="edge"
-                  boxRadius={10}
+                  boxRadius={45}
                 />
               </View>
             ))}
@@ -51,73 +51,22 @@ export const ThumbnailSkeleton = () => {
 export const HeroBannerSkeleton = () => {
   return (
     <View style={styles.heroContainer}>
-      <SkeletonLoading width="10%" height={260} />
+      <SkeletonLoading width="100%" height={460} />
       <View style={styles.heroRight}>
         <SkeletonLoading width="85%" height={260} />
       </View>
     </View>
   );
 };
-export const ReclaimSkeleton = () => (
-  <SkeletonLoading
-    width="60%"
-    height={20}
-    variant="edge"
-    boxRadius={8}
-    style={styles.SkeletonLoadingstyle}
-  />
-);
-export const GridSkeleton = ({ count = 4 }) => {
-  return (
-    <View style={styles.gridContainer}>
-      {[...Array(count)].map((_, i) => (
-        <SkeletonLoading
-          key={i}
-          width="48%"
-          height={150}
-          variant="edge"
-          boxRadius={10}
-        />
-      ))}
-    </View>
-  );
-};
-export const BannerSkeleton = () => (
-  <SkeletonLoading
-    width="100%"
-    height={200}
-    style={styles.SkeletonLoadingstyle}
-  />
-);
-export const FooterSkeleton = () => (
-  <SkeletonLoading
-    width="100%"
-    height={100}
-    style={styles.SkeletonLoadingstyle1}
-  />
-);
 export const HomeSkeleton = () => {
   return (
     <View style={styles.HomeSkeletoncontainer}>
       <ThumbnailSkeleton />
       <HeroBannerSkeleton />
-      <ReclaimSkeleton />
-      <GridSkeleton count={4} />
-      <GridSkeleton count={4} />
-      <BannerSkeleton />
-      <GridSkeleton count={6} />
-      <BannerSkeleton />
-      <FooterSkeleton />
     </View>
   );
 };
 const styles = StyleSheet.create({
-  SkeletonLoadingstyle: {
-    marginVertical: 10,
-  },
-  SkeletonLoadingstyle1: {
-    marginTop: 10,
-  },
   HomeSkeletoncontainer: {
     padding: 12,
   },
@@ -140,11 +89,5 @@ const styles = StyleSheet.create({
   },
   heroRight: {
     marginLeft: 10,
-  },
-  gridContainer: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-between',
-    marginVertical: 10,
   },
 });
